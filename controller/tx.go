5df02326d84f04a9662cca1aa5e01fbdd6897508
赵宏健 <757566@nd.com.cn>
2022-08-05 13:48:41 +0800

    feature 添加路由和方法

--- controller/tx.go
+++ controller/tx.go
@@ -32,6 +32,77 @@ func GetTx(c *gin.Context) {
 	}
 	c.IndentedJSON(res.StatusCode, response)
 }
+
+// GetTxs 获取所有的tx 如果指定block则获取所有block的tx
+func GetTxs(c *gin.Context) {
+	blockStr := c.DefaultQuery("block", "")
+	defaultSize := 20
+	sizeStr := c.DefaultQuery("size", "20")
+	size, err := strconv.Atoi(sizeStr)
+	if err != nil {
+		size = defaultSize
+	}
+	defaultPage := 1
+	pageStr := c.DefaultQuery("page", "1")
+	page, err := strconv.Atoi(pageStr)
+	if err != nil {
+		page = defaultPage
+	}
+	from := (page - 1) * size
+	body := map[string]interface{}{}
+	if blockStr != "" {
+		body = map[string]interface{}{
+			"sort": [1]interface{}{
+				map[string]interface{}{
+					"timestamp": map[string]interface{}{
+						"order": "desc",
+					},
+				},
+			},
+			"query": map[string]interface{}{
+				"match": map[string]interface{}{
+					"number": blockStr,
+				},
+			},
+		}
+	} else {
+		body = map[string]interface{}{
+			"sort": [1]interface{}{
+				map[string]interface{}{
+					"timestamp": map[string]interface{}{
+						"order": "desc",
+					},
+				},
+			},
+		}
+	}
+	var buf bytes.Buffer
+	err = json.NewEncoder(&buf).Encode(body)
+	if err != nil {
+		panic(err)
+	}
+	req := esapi.SearchRequest{
+		Index: []string{"tx"},
+		Size:  &size,
+		From:  &from,
+		Body:  &buf,
+	}
+
+	res, err := req.Do(context.Background(), db.EsClient)
+	if err != nil {
+		panic(err)
+	}
+	defer res.Body.Close()
+	var response any
+	err2 := json.NewDecoder(res.Body).Decode(&response)
+	if err2 != nil {
+		panic(err2)
+	}
+
+	c.IndentedJSON(res.StatusCode, response)
+}
+
+// GetBalanceTxs 获取交易的tx 如果指定block则获取所有block的tx
 func GetBalanceTxs(c *gin.Context) {
 	blockStr := c.DefaultQuery("block", "")
 	defaultSize := 20
@@ -112,6 +183,8 @@ func GetBalanceTxs(c *gin.Context) {
 
 	c.IndentedJSON(res.StatusCode, response)
 }
+
+// GetContractDeploy 获取合同的tx
 func GetContractDeploy(c *gin.Context) {
 	defaultSize := 20
 	sizeStr := c.DefaultQuery("size", "20")
